4894ca699e8bd0dd36f2692df21d2fe4a95019b0
Jack Allen <jack.allen@fil.com>
2018-09-26 16:48:24 +0100

    changes

--- src/routes/tasks.controller.ts
+++ src/routes/tasks.controller.ts
@@ -1,7 +1,7 @@
 import * as e from "express"
 import {Controller} from "../llama";
 import {Get} from "../llama/get";
-import { WunderlistService } from "../services/wunderlist.service";
+import {WunderlistService} from "../services/wunderlist.service";
 
 @Controller()
 export class TasksController {
@@ -23,6 +23,19 @@ export class TasksController {
             const tasks = await Promise.all(task_requests);
 
             response.send(`<script>console.log(${JSON.stringify(tasks)})</script>`);
+        }
+        catch (error) {
+            response.send('error')
+        }
+    }
+
+    @Get({path: '/:id'})
+    async task(request: e.Request, response: e.Response) {
+        const {id} = request.params;
+        try {
+            const tasks = await this.wunderlist_service.tasks(id);
+
+            response.send({ tasks })
         }
         catch (error) {
             response.send('error')
